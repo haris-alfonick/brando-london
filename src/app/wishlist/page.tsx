b762814fd65b24cf00d4c95005f57b53235c88a8
Ibad <Ibad@alfonick1.com.pk>
2024-12-11 12:46:19 +0500

    wishlist page responsive screen changes have been completed

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -11,41 +11,132 @@ const WishList = () => {
         <div className='container mx-auto sm:px-12 md:px-16 pb-8'>
           <div className='grid grid-cols-12 gap-6 pt-24'>
             <div className='col-span-12'>
-              <div className='border border-gray-300 rounded-lg p-6'>
-                <h5 className='text-lg sm:text-xl font-semibold tracking-tight text-gray-900 dark:text-white border-b pb-4'>
-                  Wishlist
-                </h5>
-                <div className="grid grid-cols-12 [&_p]:max-[485px]:text-xs bg-gray-100 py-3 px-4 border-b [&_p]:font-medium [&_p]:text-gray-700 [&_p]:text-sm [&_p]:sm:text-base">
-                  <div className="max-[780px]:col-span-3 col-span-4 md:col-span-4 sm:text-base lg:col-span-4">
+              <div className='border border-gray-300 rounded-lg [&>h3]:text-lg [&>h3]:sm:text-xl [&>h3]:font-semibold [&>h3]:text-gray-950 [&>h3]:py-3 [&>h3]:px-4 [&>h3]:max-[485px]:px-2.5'>
+                <h3>Wishlist</h3>
+                <div className="grid grid-cols-12 bg-gray-100 py-3 px-4 max-[485px]:px-2.5 border-b border-t [&_p]:font-medium [&_p]:text-gray-700 [&_p]:text-sm [&_p]:sm:text-base [&_p]:max-[485px]:text-xs">
+                  <div className="lg:col-span-5 md:col-span-4 sm:text-base col-span-4 max-[780px]:col-span-3">
                     <p>Products</p>
                   </div>
-                  <div className="max-[780px]:col-span-2 col-span-2 md:col-span-2 sm:text-base text-center lg:col-span-2">
+                  <div className="lg:col-span-2 md:col-span-2 col-span-2 max-[780px]:col-span-2">
                     <p>Price</p>
-                  </div>
-                  <div className="max-[780px]:col-span-3 col-span-2 md:col-span-3 sm:text-base text-center lg:col-span-3">
+                  </div> 
+                  <div className=" xl:col-span-3 lg:col-span-2 md:col-span-2 col-span-2 max-[780px]:col-span-3">
                     <p>Stock Status</p>
                   </div>
-                  <div className="max-[780px]:col-span-4 col-span-4 md:col-span-3 md:px-4 sm:text-base lg:col-span-3">
+                  <div className="xl:col-span-2 lg:col-span-3 md:col-span-4 col-span-4 max-[780px]:col-span-4">
                     <p>Actions</p>
                   </div>
                 </div>
 
-
-                <div className="grid grid-cols-12 py-4 px-4 items-center border-b gap-x-5 gap-y-3">
-                  <div className="max-[375px]:col-span-12 max-[468px]:col-span-12 max-[780px]:col-span-4 col-span-4 max-[640px]:col-span-12 max-[485px]:col-span-12 md:col-span-12 lg:col-span-4 sm:col-span-12 flex sm:items-center items-left gap-4 sm:flex-row [&_p]:text-gray-700 [&_p]:text-sm-marker">
+                <div className="grid grid-cols-12 py-3 px-4 max-[485px]:px-2.5 sm:items-center items-center border-b gap-y-3 max-lg:gap-x-3 max-[485px]:gap-x-0 ">
+                  
+                  <div className="lg:col-span-5 md:col-span-4 col-span-6 max-[485px]:col-span-12 flex sm:items-center items-left gap-4 sm:flex-row [&_p]:text-gray-700 max-[425px]:col-span-8">
                     <img src="/images/jacket.webp" alt="Product" className="w-12 h-14 corner" />
-                    <p className="text-sm sm:text-base sm:leading-5 !leading-4">
+                    <p className="text-sm sm:text-base sm:leading-5 !leading-[18px]">
                       Edward Cafe Racer Black Leather Jacket
                     </p>
                   </div>
-                  <div className="max-[375px]:col-span-12 max-[780px]:col-span-2 max-[468px]:col-span-2 col-span-2 max-[640px]:col-span-3 sm:col-span-3 md:col-span-3 lg:col-span-2 max-[485px]:flex-row flex-col text-center">
+
+                  <div className="lg:col-span-2 md:col-span-2 col-span-3 max-[485px]:col-span-3 max-[425px]:col-span-4 flex gap-x-1">
                     <span className='text-slate-500 line-through'>$1299</span>
                     <span>$999</span>
                   </div>
-                  <div className=" max-[375px]:col-span-12 max-[780px]:col-span-3  max-[468px]:col-span-4 col-span-2 max-[640px]:col-span-3 sm:col-span-3 max-[640px]:item-right md:col-span-3 lg:col-span-3 text-center flex items-center justify-center gap-2 [&_span]:text-[#2db224] [&_span]:uppercase">
+
+                  <div className="xl:col-span-3 lg:col-span-2 md:col-span-2 col-span-3 max-[485px]:col-span-3 max-[425px]:col-span-4 [&_span]:text-[#2db224] [&_span]:uppercase lg:[&_span]:text-base [&_span]:text-sm">
                     <span>in stock</span>
                   </div>
-                  <div className="max-[375px]:col-span-12 max-[375px]:items-center max-[780px]:col-span-4 max-[468px]:col-span-6 sm:[&_button]:w-full md:justify-center lg:justify-center col-span-4 max-[640px]:col-span-6 sm:col-span-6 md:col-span-6 lg:col-span-3 sm:justify-items-center flex gap-x-4 [&_button]:bg-[url('/images/ShoppingCartSimple.webp')] [&_button]:bg-no-repeat lg:[&_button]:bg-[right_10px_center] sm:[&_button]:bg-[right_12px_center] [&_button]:bg-[length:18px_18px] [&_button]:bg-[right_2px_center] [&_button]:uppercase md:[&_button]:pr-6 lg:[&_button]:pr-10 [&_button]:pl-4 [&_button]:py-3 [&_button]:text-[12px] [&_button]:px-8">
+
+                  <div className="xl:col-span-2 lg:col-span-3 md:col-span-4 col-span-12 max-[485px]:col-span-6 max-[425px]:col-span-8 flex gap-x-4 [&_button]:bg-[url('/images/ShoppingCartSimple.webp')] [&_button]:bg-no-repeat lg:[&_button]:bg-[right_10px_center] [&_button]:bg-[right_10px_center] [&_button]:bg-[length:16px_16px] [&_button]:uppercase [&_button]:py-2.5 lg:[&_button]:text-[13px] [&_button]:text-xs [&_button]:tracking-wide [&_button]:px-4 [&_button]:pr-8">
+                    <button type="button" className="bg-[#333333] text-white font-medium">
+                      Add to card
+                    </button>
+                    <div className="[&_svg]:w-5 [&_svg]:h-5 pt-2 justify-items-center">
+                      <FontAwesomeIcon icon={faCircleXmark} />
+                    </div>
+                  </div>
+
+                </div>
+
+                
+                <div className="grid grid-cols-12 py-3 px-4 max-[485px]:px-2.5 sm:items-center items-center border-b gap-y-3 max-lg:gap-x-3 max-[485px]:gap-x-0 ">
+                  
+                  <div className="lg:col-span-5 md:col-span-4 col-span-6 max-[485px]:col-span-12 flex sm:items-center items-left gap-4 sm:flex-row [&_p]:text-gray-700 max-[425px]:col-span-8">
+                    <img src="/images/jacket.webp" alt="Product" className="w-12 h-14 corner" />
+                    <p className="text-sm sm:text-base sm:leading-5 !leading-[18px]">
+                      Edward Cafe Racer Black Leather Jacket
+                    </p>
+                  </div>
+
+                  <div className="lg:col-span-2 md:col-span-2 col-span-3 max-[485px]:col-span-3 max-[425px]:col-span-4 flex gap-x-1">
+                    <span className='text-slate-500 line-through'>$1299</span>
+                    <span>$999</span>
+                  </div>
+
+                  <div className="xl:col-span-3 lg:col-span-2 md:col-span-2 col-span-3 max-[485px]:col-span-3 max-[425px]:col-span-4 [&_span]:text-[#2db224] [&_span]:uppercase lg:[&_span]:text-base [&_span]:text-sm">
+                    <span>in stock</span>
+                  </div>
+
+                  <div className="xl:col-span-2 lg:col-span-3 md:col-span-4 col-span-12 max-[485px]:col-span-6 max-[425px]:col-span-8 flex gap-x-4 [&_button]:bg-[url('/images/ShoppingCartSimple.webp')] [&_button]:bg-no-repeat lg:[&_button]:bg-[right_10px_center] [&_button]:bg-[right_10px_center] [&_button]:bg-[length:16px_16px] [&_button]:uppercase [&_button]:py-2.5 lg:[&_button]:text-[13px] [&_button]:text-xs [&_button]:tracking-wide [&_button]:px-4 [&_button]:pr-8">
+                    <button type="button" className="bg-[#333333] text-white font-medium">
+                      Add to card
+                    </button>
+                    <div className="[&_svg]:w-5 [&_svg]:h-5 pt-2 justify-items-center">
+                      <FontAwesomeIcon icon={faCircleXmark} />
+                    </div>
+                  </div>
+
+                </div>
+
+                
+                <div className="grid grid-cols-12 py-3 px-4 max-[485px]:px-2.5 sm:items-center items-center border-b gap-y-3 max-lg:gap-x-3 max-[485px]:gap-x-0 ">
+                  
+                  <div className="lg:col-span-5 md:col-span-4 col-span-6 max-[485px]:col-span-12 flex sm:items-center items-left gap-4 sm:flex-row [&_p]:text-gray-700 max-[425px]:col-span-8">
+                    <img src="/images/jacket.webp" alt="Product" className="w-12 h-14 corner" />
+                    <p className="text-sm sm:text-base sm:leading-5 !leading-[18px]">
+                      Edward Cafe Racer Black Leather Jacket
+                    </p>
+                  </div>
+
+                  <div className="lg:col-span-2 md:col-span-2 col-span-3 max-[485px]:col-span-3 max-[425px]:col-span-4 flex gap-x-1">
+                    <span className='text-slate-500 line-through'>$1299</span>
+                    <span>$999</span>
+                  </div>
+
+                  <div className="xl:col-span-3 lg:col-span-2 md:col-span-2 col-span-3 max-[485px]:col-span-3 max-[425px]:col-span-4 [&_span]:text-[#AB1E23] [&_span]:uppercase lg:[&_span]:text-base [&_span]:text-sm">
+                    <span>out of stock</span>
+                  </div>
+
+                  <div className="xl:col-span-2 lg:col-span-3 md:col-span-4 col-span-12 max-[485px]:col-span-6 max-[425px]:col-span-8 flex gap-x-4 [&_button]:bg-[url('/images/ShoppingCartSimple.webp')] [&_button]:bg-no-repeat lg:[&_button]:bg-[right_10px_center] [&_button]:bg-[right_10px_center] [&_button]:bg-[length:16px_16px] [&_button]:uppercase [&_button]:py-2.5 lg:[&_button]:text-[13px] [&_button]:text-xs [&_button]:tracking-wide [&_button]:px-4 [&_button]:pr-8">
+                    <button type="button" className="bg-[#333333] text-white font-medium">
+                      Add to card
+                    </button>
+                    <div className="[&_svg]:w-5 [&_svg]:h-5 pt-2 justify-items-center">
+                      <FontAwesomeIcon icon={faCircleXmark} />
+                    </div>
+                  </div>
+
+                </div>
+
+                
+                <div className="grid grid-cols-12 py-3 px-4 max-[485px]:px-2.5 sm:items-center items-center border-b gap-y-3 max-lg:gap-x-3 max-[485px]:gap-x-0 ">
+                  
+                  <div className="lg:col-span-5 md:col-span-4 col-span-6 max-[485px]:col-span-12 flex sm:items-center items-left gap-4 sm:flex-row [&_p]:text-gray-700 max-[425px]:col-span-8">
+                    <img src="/images/jacket.webp" alt="Product" className="w-12 h-14 corner" />
+                    <p className="text-sm sm:text-base sm:leading-5 !leading-[18px]">
+                      Edward Cafe Racer Black Leather Jacket
+                    </p>
+                  </div>
+
+                  <div className="lg:col-span-2 md:col-span-2 col-span-3 max-[485px]:col-span-3 max-[425px]:col-span-4 flex gap-x-1">
+                    <span className='text-slate-500 line-through'>$1299</span>
+                    <span>$999</span>
+                  </div>
+
+                  <div className="xl:col-span-3 lg:col-span-2 md:col-span-2 col-span-3 max-[485px]:col-span-3 max-[425px]:col-span-4 [&_span]:text-[#2db224] [&_span]:uppercase lg:[&_span]:text-base [&_span]:text-sm">
+                    <span>in stock</span>
+                  </div>
+
+                  <div className="xl:col-span-2 lg:col-span-3 md:col-span-4 col-span-12 max-[485px]:col-span-6 max-[425px]:col-span-8 flex gap-x-4 [&_button]:bg-[url('/images/ShoppingCartSimple.webp')] [&_button]:bg-no-repeat lg:[&_button]:bg-[right_10px_center] [&_button]:bg-[right_10px_center] [&_button]:bg-[length:16px_16px] [&_button]:uppercase [&_button]:py-2.5 lg:[&_button]:text-[13px] [&_button]:text-xs [&_button]:tracking-wide [&_button]:px-4 [&_button]:pr-8">
                     <button type="button" className="bg-[#333333] text-white font-medium">
                       Add to card
                     </button>
@@ -61,9 +152,68 @@ const WishList = () => {
           </div>
         </div>
       </div>
+
+      
       <Footer />
     </>
   )
 }
 
 export default WishList
+
+
+
+// "use client"
+
+// const Animation = () => {
+//   return (
+//     <>
+//       <div className="w-full h-[400px] flex pt-5">
+//         <div className="w-1/2 h-full overflow-hidden relative">
+//           <div className="absolute top-0 left-0 w-full h-[200%] animate-slide-up">
+//             <img src="/images/image-6.webp" alt="Image 1" className="w-full h-1/2 object-cover"/>
+//             <img src="/images/image-5.webp" alt="Image 2" className="w-full h-1/2 object-cover"/>
+//           </div>
+//         </div>
+
+//         <div className="w-1/2 h-full overflow-hidden relative">
+//           <div className="absolute top-[-100%] left-0 w-full h-[200%] animate-slide-down">
+//             <img src="/images/image-7.webp" alt="Image 3" className="w-full h-1/2 object-cover"/>
+//             <img src="/images/image-8.webp" alt="Image 4" className="w-full h-1/2 object-cover"/>
+//           </div>
+//         </div>
+//       </div>
+
+//       <style jsx>{`
+//         @keyframes slide-up {
+//           0%,
+//           100% {
+//             transform: translateY(0);
+//           }
+//           50% {
+//             transform: translateY(-50%);
+//           }
+//         }
+//         @keyframes slide-down {
+//           0%,
+//           100% {
+//             transform: translateY(-50%);
+//           }
+//           50% {
+//             transform: translateY(0);
+//           }
+//         }
+//         .animate-slide-up {
+//           animation: slide-up 7s infinite;
+//         }
+//         .animate-slide-down {
+//           animation: slide-down 7s infinite;
+//         }
+//       `}</style>
+//     </>
+//   );
+// };
+
+// export default Animation;
+
+
