34776dda8a3d3734b1627c83710b7727b2484c7a
mhmdharis <codeaccuracy@gmail.com>
2025-01-17 15:41:19 +0500

    something

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,20 +1,53 @@
-import BreadCrumbs from '../components/breadcrumbs'
-import Navbar from '../components/Navbar/Navbar'
-import Footer from '../components/Footer'
-import BillingInformation from './billingInformation'
-import PaymentOptions from './paymentOptions'
-import AdditionalInformation from './additionalInformation'
-import OrderSummary from './orderSummary'
-import StripeCheckOut from '../components/stripe-checkout'
+"use client";
+
+import BreadCrumbs from "../components/breadcrumbs";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer";
+import StripeCheckOut from "../components/stripe-checkout";
+import { useAppSelector } from "@/lib/hooks";
+import { useEffect, useState } from "react";
+import { updateCartAndCalculateTotals } from "@/utils/cartUtils";
 
 const Checkout = () => {
+  const cartItemsData = useAppSelector((state) => state.cart.items);
+  const [totals, setTotals] = useState({ subtotal: 0, shipping: 0, total: 0 });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchCartData = async () => {
+      setLoading(true);
+      setError("");
+
+      try {
+        const { subtotal, shipping, total } =
+          await updateCartAndCalculateTotals(cartItemsData);
+        setTotals({ subtotal, shipping, total });
+      } catch (err) {
+        setError("Failed to update cart. Please try again.");
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCartData();
+  }, [cartItemsData]);
+
   return (
     <div>
       <Navbar />
-      <BreadCrumbs page='Checkout' />
-      <StripeCheckOut />
+      <BreadCrumbs page="Checkout" />
+      {loading ? (
+        <p>Loading your cart...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <StripeCheckOut total={totals.total} />
+      )}
       <Footer />
     </div>
-  )
-}
-export default Checkout
+  );
+};
+
+export default Checkout;
--- src/app/components/WomenJackets.tsx
+++ src/app/components/WomenJackets.tsx
@@ -14,7 +14,7 @@ const Womenjackets = async () => {
           </div>
           <div className='grid grid-cols-12 gap-x-7 md:gap-y-0 gap-y-3'>
             {womenJackets.map((product: Product) => (
-              <ProductCard key={product.id} product={product} />
+              <ProductCard key={product.id} product={product} relatedProduct={false} />
             ))}
           </div>
         </div>
