33f2c0ce9d7d88e698e14624397402086c9e56d5
mhmdharis <codeaccuracy@gmail.com>
2025-04-14 18:27:35 +0500

    test

--- src/app/api/stripe-payment-intent/route.ts
+++ src/app/api/stripe-payment-intent/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!); 
 
 export const POST = async (request: NextRequest) => {
   try {
