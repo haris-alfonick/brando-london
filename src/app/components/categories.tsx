c8b3e5fc39b170434a1dce698a7135ba73c48ac7
Ali Ayoub <aliayoubpaya@gmail.com>
2025-06-25 21:58:48 +0500

    homepage category images has been changed, and the homepage section tabs button has been styled

--- src/app/components/categories.tsx
+++ src/app/components/categories.tsx
@@ -11,42 +11,42 @@ const CategoryList = async () => {
           <div className="animateCircle relative z-[9999] after:animate-spin after:z-[-1] after:-top-20 after:left-0 after:right-0 after:m-auto grid grid-cols-12 md:pt-10 pt-7 gap-x-5 gap-y-5 md:gap-y-0 [&_strong]:block [&_strong]:bg-[#0D0D0D]/60 [&_strong]:text-white [&_strong]:py-3 [&_strong]:absolute [&_strong]:bottom-0 [&_strong]:w-full [&_strong]:left-0 [&_strong]:text-center [&_strong]:md:text-lg [&_strong]:font-medium">
             <Link href="/mens-jackets" className="relative item1 md:col-span-3 col-span-6">
               <Image
-                src="/images/mens-jackets.jpg"
+                src="/images/men-pilot-jackets.webp"
                 alt="Image"
-                width={500}
+                width={820}
                 height={100}
                 className="w-full h-auto" />
-              <strong>Men&apos;s Jackets</strong>
+              <strong>Men&apos;s Pilot Jackets</strong>
             </Link>
 
             <Link href="/womens-jackets" className="relative item2 md:col-span-3 col-span-6">
               <Image
-                src="/images/womens-jackets.jpg"
+                src="/images/women-pilot-jackets.webp"
                 alt="Image"
-                width={500}
+                width={820}
                 height={100}
                 className="w-full h-auto" />
-              <strong>Women&apos;s Jackets</strong>
+              <strong>Women&apos;s Pilot Jackets</strong>
             </Link>
 
             <Link href="/mens-coats" className="relative item3 md:col-span-3 col-span-6">
               <Image
-                src="/images/mens-coats.jpg"
+                src="/images/men-biker-jackets.webp"
                 alt="Image"
-                width={500}
+                width={820}
                 height={100}
                 className="w-full h-auto" />
-              <strong>Men&apos;s Coats</strong>
+              <strong>Men&apos;s Biker Jackets</strong>
             </Link>
 
             <Link href="/womens-coats" className="relative item4 md:col-span-3 col-span-6">
               <Image
-                src="/images/womens-coats.jpg"
+                src="/images/women-casual-jackets.webp"
                 alt="Image"
-                width={500}
+                width={820}
                 height={100}
                 className="w-full h-auto" />
-              <strong>Women&apos;s Coats</strong>
+              <strong>Women&apos;s Casual Jackets</strong>
             </Link>
           </div>
         </div>
